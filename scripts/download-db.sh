02defaba1660aad083ea9a377b222bccbf7a772e
Sven Werlen <sven.werlen@pm.me>
2020-05-05 19:49:05 -0400

    Classes, feats, equipment and spells

--- scripts/download-db.sh
+++ scripts/download-db.sh
@@ -1,2 +1,6 @@
 !/bin/bash
 curl https://gitlab.com/hooking/foundry-vtt---pathfinder-2e/-/raw/master/dist/packs/actions.db > ../packs/actions.db
+curl https://gitlab.com/hooking/foundry-vtt---pathfinder-2e/-/raw/master/dist/packs/classes.db > ../packs/classes.db
+curl https://gitlab.com/hooking/foundry-vtt---pathfinder-2e/-/raw/master/dist/packs/feats.db > ../packs/feats.db
+curl https://gitlab.com/hooking/foundry-vtt---pathfinder-2e/-/raw/master/dist/packs/spells.db > ../packs/spells.db
+curl https://gitlab.com/hooking/foundry-vtt---pathfinder-2e/-/raw/master/dist/packs/equipment.db > ../packs/equipment.db
--- scripts/extract.py
+++ scripts/extract.py
@@ -1,23 +1,45 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-ROOT="../"
-FILE=ROOT + "packs/actions.db"
-
 import json
-
-# read file
-with open(FILE, 'r') as f:
-  content = f.readlines()
+import yaml
+import os
 
 
-for line in content:
-  obj = json.loads(line)
-  
-  with open(ROOT + 'data/actions/' + obj['_id'] + ".htm", 'w') as df:
-    df.write('Name: ' + obj['name'] + '\n')
-    df.write('Nom: ' + '\n')
-    df.write('État: aucune\n\n')
-    df.write('------ Description (en) ------' + '\n')
-    df.write(obj['data']['description']['value'] + '\n')
-    df.write('------ Description (fr) ------' + '\n')
+FILES = ["actions", "classes", "equipment", "feats", "spells"]
+ROOT="../"
+
+for F in FILES:
+  FILE=ROOT + "packs/" + F + ".db"
+
+  # read file
+  with open(FILE, 'r') as f:
+    content = f.readlines()
+
+
+  for line in content:
+    obj = json.loads(line)
+    
+    filepath = ROOT + 'data/' + F + '/' + obj['_id'] + ".htm"
+    if os.path.isfile(filepath):
+      with open(filepath, 'r') as df:
+        content = df.read()
+        metadata = yaml.load(content.split('------ Description (en) ------')[0])
+        if metadata['État'] != 'aucune':
+          continue
+    
+    if not 'name' in obj:
+        continue
+      
+    with open(filepath, 'w') as df:
+      df.write('Name: ' + obj['name'] + '\n')
+      df.write('Nom: ' + '\n')
+      df.write('État: aucune\n\n')
+      df.write('------ Description (en) ------' + '\n')
+      
+      if F in ("actions", "equipment", "feats", "spells"):
+        df.write(obj['data']['description']['value'] + '\n')
+      
+      elif F == "classes":
+        df.write(obj['content'] + '\n')
+      df.write('------ Description (fr) ------' + '\n')
