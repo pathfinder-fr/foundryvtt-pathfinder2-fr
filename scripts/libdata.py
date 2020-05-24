e6941c3a20b86b5ef57030ab399886feffd543cf
Sven Werlen <sven.werlen@pm.me>
2020-05-24 13:59:59 -0400

    New actions

--- scripts/libdata.py
+++ scripts/libdata.py
@@ -8,7 +8,7 @@ import os
 # (ignore les retours à la ligne)
 #
 def equals(val1, val2):
-  return val1.replace('\n','').strip() == val2.replace('\n','').strip()
+  return val1.replace('\n','').replace('\r', '').strip() == val2.replace('\n','').replace('\r', '').strip()
 
 #
 # cette fonction tente une extraction d'une valeur dans un objet
@@ -84,20 +84,14 @@ def fileToData(filepath):
 #
 def dataToFile(data, filepath):
 
-  if os.path.isfile(filepath):
-    
-    with open(filepath, 'w') as df:
-      df.write('Name: ' + data['nameEN'] + '\n')
-      df.write('Nom: ' + data['nameFR'] + '\n')
-      df.write('État: ' + data['status'] + '\n\n')
-      df.write('------ Description (en) ------' + '\n')
-      df.write(data['descrEN'] + '\n')
-      df.write('------ Description (fr) ------' + '\n')
-      if len(data['descrFR']) > 0:
-        df.write(data['descrFR'] + '\n')
-    
-  else:
-    print("Invalid path: %s" % filepath)
-    exit(1)
-  
+  with open(filepath, 'w') as df:
+    df.write('Name: ' + data['nameEN'] + '\n')
+    df.write('Nom: ' + data['nameFR'] + '\n')
+    df.write('État: ' + data['status'] + '\n\n')
+    df.write('------ Description (en) ------' + '\n')
+    df.write(data['descrEN'] + '\n')
+    df.write('------ Description (fr) ------' + '\n')
+    if len(data['descrFR']) > 0:
+      df.write(data['descrFR'] + '\n')
+
   return data
