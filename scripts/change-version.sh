f287de71497b24291f918cc53af4f079c73645e5
Sven Werlen <sven.werlen@pm.me>
2020-06-20 15:19:47 -0400

    v-0.27.1

--- scripts/change-version.sh
+++ scripts/change-version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 CURVER=`grep "version" ../module.json | awk -F'.' '{print $2}'`
 NEWVER="$(($CURVER+1))"
-cat ../module.template.json | sed "s/VERSION/0.$NEWVER.0/g" >> ../module.json
+cat ../module.template.json | sed "s/VERSION/0.$NEWVER.0/g" > ../module.json
 echo "Version: 0.$NEWVER.0"
 
