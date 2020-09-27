9176b9f133d7346c603bd41a13789ec3b61311cd
Sven Werlen <sven.werlen@pm.me>
2020-09-27 13:24:40 -0400

    v-0.65.0

--- scripts/change-version.sh
+++ scripts/change-version.sh
@@ -2,6 +2,7 @@
 CURVER=`grep "version" ../module.json | awk -F'.' '{print $2}'`
 NEWVER="$(($CURVER+1))"
 cat ../module.template.json | sed "s/VERSION/0.$NEWVER.0/g" > ../module.json
+git add ../module.json
 VERSION="v-0.$NEWVER.0"
 echo "git commit -m \"$VERSION\" && git tag \"$VERSION\" && git push && git push --tags"
 
