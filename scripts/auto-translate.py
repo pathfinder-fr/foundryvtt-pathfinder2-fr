16a76cc341894b7ce45b0ad03cdcc26c71eb303d
Nat-Faeeria <contact@nathanael-gimenez.com>
2021-04-09 13:53:20 +0200

    changement de la méthode de génération automatique de traduction

--- scripts/auto-translate.py
+++ scripts/auto-translate.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-
+import logging
 from libdata import *
+
+logging.basicConfig(filename='translation.log', level=logging.INFO)
+
 
 ROOT = "../"
 packs = getPacks()
@@ -17,14 +20,16 @@ for p in packs:
     for id in existing:
         filename = existing[id]["filename"]
         filepath = "%sdata/%s/%s" % (ROOT, p["id"], filename)
-        if existing[id]["status"] == "aucune":
+        if existing[id]["status"] == "auto-trad":
             name = existing[id]["nameEN"]
-            print("Translating "+name)
+            logging.info("Translating "+name)
             toTrad = existing[id]['descrEN']
             try:
                 translated = dirtyTranslate(toTrad)
                 existing[id]['descrFR'] = translated
                 existing[id]['status'] = "auto-trad"
                 dataToFile(existing[id], filepath)
+                logging.info("Success !")
             except Exception as e:
-                print("Error while translating "+name+" : "+type(e).__name__)
+                logging.error("Error while translating "+name+" : "+type(e).__name__)
+                logging.error(e)
--- scripts/libdata.py
+++ scripts/libdata.py
@@ -5,9 +5,11 @@ import os
 import re
 import requests
 import json
+import html
 
 from selenium import webdriver
-from selenium.webdriver import DesiredCapabilities
+from selenium.webdriver.support import expected_conditions as ExpectedConditions
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
 
@@ -281,35 +283,34 @@ def readFolder(path):
 
 #
 # Cette fonction tente une traduction automatique 
-# sur Google Translate
+# sur DeepL
 #
 def dirtyTranslate(data):
-  options = webdriver.FirefoxOptions()
-  options.add_argument("--headless")
-  driver = webdriver.Firefox(options=options)
-  driver.get("https://translate.google.com/?sl=en&tl=fr&format=html&op=translate")
-  inputer = driver.find_element_by_xpath("//textarea[@jsname='BJE2fc']")
-  inputer.send_keys(data)
-  output = WebDriverWait(driver, timeout=40).until(lambda d: d.find_element_by_xpath("//div[@class='J0lOec']"))
-  transData = output.text
-  driver.quit()
-  transData = cleanTrad(transData)
-  return transData
+    options = webdriver.FirefoxOptions()
+    options.add_argument("--headless")
+    driver = webdriver.Firefox(options=options)
+    try:
+        driver.get("https://www.deepl.com/en/translator#en/fr/stuff")
+        inputer = driver.find_element_by_class_name('lmt__source_textarea')
+        inputer.clear()
+        inputer.send_keys(html.escape(data))
+        WebDriverWait(driver, 40).until(
+            ExpectedConditions.text_to_be_present_in_element_value((By.CLASS_NAME, 'lmt__target_textarea'), "p"))
+        output = driver.find_element_by_class_name('lmt__target_textarea')
+        transData = html.unescape(output.get_attribute("value"))
+        driver.quit()
+        transData = cleanTrad(transData)
+        return transData
+    except Exception as e:
+        driver.quit()
+        raise
+
 
 #
 # Nettoie la traduction en corrigeant les termes mal-traduits ou
 # qui ne devaient pas être traduits
 #
 def cleanTrad(data):
-  data = data.replace("</ ", "</")
-  data = data.replace("<Strong>", "<strong>")
-  data = data.replace("</Strong>", "</strong>")
-  data = re.sub("<[f,F]+ort[e]{0,1}>", "<strong>", data)
-  data = re.sub("</[f,F]+ort[e]{0,1}>", "</strong>", data)
-  data = re.sub("[0-9]+D[0-9]+", lambda s: s.group(0).replace("D", "d"), data)
-  data = data.replace("</P>", "</p>")
-  data = data.replace("[[/ R", "[[/r")
-  data = data.replace("accroître", "Intensifié")
-  data = data.replace("#feu", "#fire")
-  data = data.replace("#acide", "#acid")
-  return data
+    data = data.replace("</p>>", "<p>")
+    data = data.replace("Traduit avec www.DeepL.com/Translator (version gratuite)", "")
+    return data
