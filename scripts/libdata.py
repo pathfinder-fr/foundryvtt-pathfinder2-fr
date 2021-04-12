7a87736f56544748e7c68e671ab8601e31764eed
Nat-Faeeria <contact@nathanael-gimenez.com>
2021-04-13 00:47:55 +0200

    Ajout de conditions pour la traduction automatique

--- scripts/libdata.py
+++ scripts/libdata.py
@@ -330,6 +330,10 @@ def dirtyTranslate(driver, data):
 #
 def cleanTrad(data):
     data = data.replace("&gt ;", "&gt")
+    data = data.replace("Activate", "Activation")
+    data = data.replace("Interact", "Interagir")
+    data = data.replace("60 feet", "18 mètres")
+    data = data.replace("30 feet", "9 mètres")
     data = data.replace("Traduit avec www.DeepL.com/Translator (version gratuite)", "")
     return data
 
