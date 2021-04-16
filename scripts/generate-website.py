0c701889259d6483925d004e2d5b235848ce41a6
Nat-Faeeria <contact@nathanael-gimenez.com>
2021-04-16 02:41:29 +0200

    Changed generation for pf2-data-fr

--- scripts/generate-website.py
+++ scripts/generate-website.py
@@ -15,53 +15,51 @@ packs = getPacks()
 for p in packs:   
   
   list = []
+    
+  translations = {}
   
-  if p["name"] == "actionspf2e":
+  #############################################
+  # read all available data for specified pack
+  #############################################
+  path = "../data/%s/" % p["id"]
+  all_files = os.listdir(path)
+  for fpath in all_files:
     
-    translations = {}
-    
-    #############################################
-    # read all available data for specified pack
-    #############################################
-    path = "../data/%s/" % p["id"]
-    all_files = os.listdir(path)
-    for fpath in all_files:
-      
-      data = fileToData(path + fpath)
-      if data['status'] == 'aucune' or data['status'] == "auto-trad" \
-        or data['status'] == "auto-googtrad"  or data['status'] == "vide":
-        continue
-          
-      # default (all translations in french)
-      translations[data['id']] = { 'name': data['nameFR'], 'description': data['descrFR'], 'status': data['status'] }
-
-    #############################################
-    # read original data from pf2 Foundry system
-    #############################################
-    with open(PACKS + p["id"] + ".db", 'r') as f:
-      content = f.readlines()
-
-    count = 0
-    for line in content:
-      count += 1
-      try:
-        obj = json.loads(line)
-      except:
-        print("Invalid json %s at line %d" % (FILE, count))
-        continue
-      
-      if '$$deleted' in obj:
-        continue
-    
-      # get translations
-      obj['translations'] = { 'fr': { 'status': 'aucune' } }
-      if obj['_id'] in translations:
-        el =  translations[obj['_id']]
-        obj['translations']['fr']['status'] = el['status']
-        obj['translations']['fr']['name'] = el['name']
-        obj['translations']['fr']['description'] = el['description']
+    data = fileToData(path + fpath)
+    if data['status'] == 'aucune' or data['status'] == "auto-trad" \
+      or data['status'] == "auto-googtrad"  or data['status'] == "vide":
+      continue
         
-      list.append(obj)
+    # default (all translations in french)
+    translations[data['id']] = { 'name': data['nameFR'], 'description': data['descrFR'], 'status': data['status'] }
 
-    with open(WEBSITE_DATA + "actions.json", 'w') as outfile:
-      json.dump(list, outfile, indent=3)
+  #############################################
+  # read original data from pf2 Foundry system
+  #############################################
+  with open(PACKS + p["id"] + ".db", 'r') as f:
+    content = f.readlines()
+
+  count = 0
+  for line in content:
+    count += 1
+    try:
+      obj = json.loads(line)
+    except:
+      print("Invalid json %s at line %d" % (FILE, count))
+      continue
+    
+    if '$$deleted' in obj:
+      continue
+  
+    # get translations
+    obj['translations'] = { 'fr': { 'status': 'aucune' } }
+    if obj['_id'] in translations:
+      el =  translations[obj['_id']]
+      obj['translations']['fr']['status'] = el['status']
+      obj['translations']['fr']['name'] = el['name']
+      obj['translations']['fr']['description'] = el['description']
+      
+    list.append(obj)
+
+  with open(WEBSITE_DATA + "actions.json", 'w') as outfile:
+    json.dump(list, outfile, indent=3)
