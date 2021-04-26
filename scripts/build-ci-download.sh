4adb19e25bd895afcf65b462987dd6e7f2d78653
Thomas Bolon <tbolon@mb3m.fr>
2021-04-26 16:27:08 +0200

    modif téléchargement

---
+++ scripts/build-ci-download.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+echo "Downloading latest foundry pack"
+mkdir .ci/ || true
+wget -q https://gitlab.com/hooking/foundry-vtt---pathfinder-2e/-/jobs/artifacts/release/raw/pf2e.zip?job=build -O .ci/pf2e.zip
+unzip -q -o -d .ci/ .ci/pf2e.zip
+mv .ci/pf2e/packs ../packs
+
+echo "Done"
