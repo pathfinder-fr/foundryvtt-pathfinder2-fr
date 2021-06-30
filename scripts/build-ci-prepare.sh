d2f644c3e32f5ce213bffdd1cb090cce39818a9d
Nat-Faeeria <contact@nathanael-gimenez.com>
2021-06-30 21:44:43 +0000

    Update build-ci-prepare.sh

--- scripts/build-ci-prepare.sh
+++ scripts/build-ci-prepare.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-chmod a+x test-ci.py
-echo "Test stdout python inline"
-python3 test-ci.py
-echo "Test stdout"
-./test-ci.py
-
 echo "Extracting data and updating translation files"
 ./prepare.py
 
