c4c2283ff172d21882e8d75e0f99f45f1f417f68
Nat-Faeeria <contact@nathanael-gimenez.com>
2021-10-02 13:22:52 +0000

    Fix versionning

--- scripts/build-ci-module.sh
+++ scripts/build-ci-module.sh
@@ -12,7 +12,7 @@ echo "Increment module version"
 CURVER=`grep "version" ../module.json | awk -F'.' '{print $2}'`
 NEWVER="$(($CURVER+1))"
 VERSION="v-1.$NEWVER.0"
-cat ../module.template.json | sed "s/VERSION/0.$NEWVER.0/g" > ../module.json
+cat ../module.template.json | sed "s/VERSION/1.$NEWVER.0/g" > ../module.json
 
 if [ $CI_DEPLOY_MODULE = "true" ]
 then
