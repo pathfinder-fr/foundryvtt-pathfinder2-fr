c6e0418fce068663f27ac713afc2bf23a5bffd5b
Sven Werlen <sven.werlen@pm.me>
2022-01-16 11:51:02 -0500

    Fix download pour V9

--- scripts/build-ci-download.sh
+++ scripts/build-ci-download.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Downloading latest foundry pack"
 mkdir .ci/ || true
-wget -q https://gitlab.com/hooking/foundry-vtt---pathfinder-2e/-/jobs/artifacts/release-0.8.x/raw/pf2e.zip?job=build -O .ci/pf2e.zip
+wget -q https://gitlab.com/hooking/foundry-vtt---pathfinder-2e/-/jobs/artifacts/release-v9/download?job=build -O .ci/pf2e.zip
 unzip -q -o -d .ci/ .ci/pf2e.zip
 mv .ci/pf2e/packs ../packs
 
