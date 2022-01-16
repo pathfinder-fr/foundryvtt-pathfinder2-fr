834bc0b5e04fd9930d2482bb33fa3eb57e1d4d26
Sven Werlen <sven.werlen@pm.me>
2022-01-16 12:05:15 -0500

    Corriger pour nouveau format de ZIP

--- scripts/build-ci-download.sh
+++ scripts/build-ci-download.sh
@@ -5,7 +5,8 @@ set -e
 echo "Downloading latest foundry pack"
 mkdir .ci/ || true
 wget -q https://gitlab.com/hooking/foundry-vtt---pathfinder-2e/-/jobs/artifacts/release-v9/download?job=build -O .ci/pf2e.zip
-unzip -q -o -d .ci/ .ci/pf2e.zip
+unzip -o -d .ci/ .ci/pf2e.zip # ZIP initial ne contient qu'un autre ZIP (pf2e.zip et le manifest JSON)
+unzip -o -d .ci/ .ci/pf2e.zip # ZIP du système
 mv .ci/pf2e/packs ../packs
 
 echo "Done"
